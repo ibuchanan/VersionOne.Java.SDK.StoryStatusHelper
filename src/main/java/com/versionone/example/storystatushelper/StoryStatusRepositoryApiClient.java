cbee6c0b60c23b05f9f55fe28137fc1dabf99bf7
Ian Buchanan <ian.buchanan@versionone.com>
2013-06-22 21:16:22 -0400

    query selects name and changedate

--- src/main/java/com/versionone/example/storystatushelper/StoryStatusRepositoryApiClient.java
+++ src/main/java/com/versionone/example/storystatushelper/StoryStatusRepositoryApiClient.java
@@ -3,16 +3,21 @@ package com.versionone.example.storystatushelper;
 import com.versionone.DB.DateTime;
 import com.versionone.apiclient.EnvironmentContext;
 import com.versionone.apiclient.IAssetType;
+import com.versionone.apiclient.IAttributeDefinition;
 import com.versionone.apiclient.Query;
 
 public class StoryStatusRepositoryApiClient implements StoryStatusRepository {
 	
 	private DateTime mostRecentChangeDateTime;
 	private IAssetType storyStatusType;
+	private IAttributeDefinition nameAttribute;
+	private IAttributeDefinition changeAttribute;
 
 	public StoryStatusRepositoryApiClient(EnvironmentContext cx) {
 		mostRecentChangeDateTime = null;
 		storyStatusType = cx.getMetaModel().getAssetType("StoryStatus");
+		nameAttribute = storyStatusType.getAttributeDefinition("Name");
+		changeAttribute = storyStatusType.getAttributeDefinition("ChangeDateUTC");
 	}
 
 	public boolean isDirty() {
@@ -24,6 +29,8 @@ public class StoryStatusRepositoryApiClient implements StoryStatusRepository {
 
 	public Query buildQueryForAllStoryStatuses() {
 		Query query = new Query(storyStatusType);
+		query.getSelection().add(nameAttribute);
+		query.getSelection().add(changeAttribute);
         return query;
 	}
 
--- src/test/java/com/versionone/example/storystatushelper/StoryStatusRepositoryTest.java
+++ src/test/java/com/versionone/example/storystatushelper/StoryStatusRepositoryTest.java
@@ -3,6 +3,8 @@ package com.versionone.example.storystatushelper;
 import static org.junit.Assert.*;
 
 import com.versionone.apiclient.EnvironmentContext;
+import com.versionone.apiclient.IAssetType;
+import com.versionone.apiclient.IAttributeDefinition;
 import com.versionone.apiclient.Query;
 
 import org.junit.Test;
@@ -13,36 +15,78 @@ import org.junit.Test;
 public class StoryStatusRepositoryTest {
 
 	@Test
-    public void new_story_status_repository_is_dirty()
-    {
-    	// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
-    	EnvironmentContext cx = null;
-    	try {
+	public void new_story_status_repository_is_dirty() {
+		// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
+		EnvironmentContext cx = null;
+		try {
 			cx = new EnvironmentContext();
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
-        // When I create a new repository with that connection
-    	StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
-        // Then it is initially dirty
-        assertTrue( repository.isDirty() );
-    }
-	
-    @Test
-    public void query_for_story_statuses_is_scoped_to_StoryStatus()
-    {
-    	// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
-    	EnvironmentContext cx = null;
-    	try {
+		// When I create a new repository with that connection
+		StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
+		// Then it is initially dirty
+		assertTrue(repository.isDirty());
+	}
+
+	@Test
+	public void query_for_story_statuses_is_scoped_to_StoryStatus() {
+		// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
+		EnvironmentContext cx = null;
+		try {
 			cx = new EnvironmentContext();
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
-        // And a new repository with that connection
-    	StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
-        // When I build the query for story statuses
-        Query query = repository.buildQueryForAllStoryStatuses();
-        // Then the asset type is StoryStatus
-        assertEquals("StoryStatus", query.getAssetType().getToken());
-    }
+		// And a new repository with that connection
+		StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
+		// When I build the query for story statuses
+		Query query = repository.buildQueryForAllStoryStatuses();
+		// Then the asset type is StoryStatus
+		assertEquals("StoryStatus", query.getAssetType().getToken());
+	}
+
+	@Test
+	public void query_for_request_categories_selects_name() {
+		// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
+		EnvironmentContext cx = null;
+		try {
+			cx = new EnvironmentContext();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		// And a new repository with that connection
+		StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
+		// And a reference to the story status asset type
+		IAssetType assetType = cx.getMetaModel().getAssetType("StoryStatus");
+		// And a reference to the name attribute
+		IAttributeDefinition nameAttribute = assetType.getAttributeDefinition("Name");
+		// When I build the query for request categories
+		Query query = repository.buildQueryForAllStoryStatuses();
+		// Then the query selects the name attribute
+		assertTrue(query.getSelection().contains(nameAttribute));
+	}
+
+	@Test
+	public void query_for_request_categories_selects_change_date() {
+		// Given a connection to a VersionOne instance defined in the APIConfiguration.properties
+		EnvironmentContext cx = null;
+		try {
+			cx = new EnvironmentContext();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		// And a new repository with that connection
+		StoryStatusRepository repository = new StoryStatusRepositoryApiClient(cx);
+		// And a reference to the story status asset type
+		IAssetType assetType = cx.getMetaModel().getAssetType("StoryStatus");
+		// And a reference to the name attribute
+		IAttributeDefinition nameAttribute = assetType.getAttributeDefinition("ChangeDateUTC");
+		// When I build the query for request categories
+		Query query = repository.buildQueryForAllStoryStatuses();
+		// Then the query selects the name attribute
+		assertTrue(query.getSelection().contains(nameAttribute));
+	}
+
+
 }
